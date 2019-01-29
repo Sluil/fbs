6869b643be8e56bc6680849852e193ff2ab926b9
Matthew Sandrock <msandrock@hotmail.com>
2019-01-29 01:13:53 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer>
+	<div id="copyright">
+	<?php
+		$copyYear = 2013; // start date
+		$curYear = date('Y'); // second year
+		echo $copyYear.(($copyYear != $curYear)? '-'.$curYear:'');
+	?>
+	Copyright.
+</div>
+	<div id="sources">
+		<a href=sources.php">Sources</a>
+	</div>
+	
+	<div id="company-name">
+		<span>Harry's Hot Sauce</spam>
+	</div>
+</footer>
---
+++ header.php
@@ -0,0 +1,5 @@
+<header>
+	<div id="logo">
+		<img src="images/logo.png" alt="Harry's Hot Sauce"/>
+	</div>
+</header>
